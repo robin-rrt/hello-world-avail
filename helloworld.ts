a32f3659d29924da50360565666d9918ae261dbf
robin-rrt <66296664+robin-rrt@users.noreply.github.com>
2024-10-29 21:09:14 +0530

    extract data

--- helloworld.ts
+++ helloworld.ts
@@ -28,7 +28,26 @@ const main = async () => {
       console.log(`Transaction was not executed`)
       process.exit(1)
     }
- 
+    const [txHash, blockHash] = [txResult.txHash as H256, txResult.status.asFinalized as H256]
+    console.log(`Tx Hash: ${txHash}, Block Hash: ${blockHash}`)
+
+    // Extracting data
+    const block = await api.rpc.chain.getBlock(blockHash)
+    const tx = block.block.extrinsics.find((tx) => tx.hash.toHex() == txHash.toHex())
+    if (tx == undefined) {
+      console.log("Failed to find the Submit Data transaction")
+      process.exit(1)
+    }
+
+    console.log(JSON.stringify(tx))
+    const dataHex = tx.method.args.map((a) => a.toString()).join(", ")
+    // Data retrieved from the extrinsic data
+    let str = ""
+    for (let n = 0; n < dataHex.length; n += 2) {
+      str += String.fromCharCode(parseInt(dataHex.substring(n, n + 2), 16))
+    }
+    console.log(`submitted data: ${str}`)
+
   process.exit()
 } catch (err) {
     console.error(err)
